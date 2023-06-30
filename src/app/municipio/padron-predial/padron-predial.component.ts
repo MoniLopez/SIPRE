e80652b6e8d2efe3cd3c5bcfb9d993ce0d7d9bea
MoniLopez <133059943+MoniLopez@users.noreply.github.com>
2023-06-30 17:00:06 -0600

    Recibe datos de las cuentas valuadas y las muestra en una tabla del componente padronPredial close #6

--- src/app/municipio/padron-predial/padron-predial.component.ts
+++ src/app/municipio/padron-predial/padron-predial.component.ts
@@ -9,68 +9,17 @@ import { DatosMpioService } from '../../service/datos-mpio.service'; //Llama ser
   styleUrls: ['./padron-predial.component.css']
 })
 export class PadronPredialComponent {
-  constructor(private router: Router, private service: DatosMpioService){}
-
-  //cuentas: Cuentas[] = [];
-  btnAceptar(){
-    //this.router.navigate(['dashboard/municipios/selecMpio']); //Mueve a la pagina que indica el router
+  constructor(private router: Router, private service: DatosMpioService){
+    this.obtenerDatos();
   }
 
-  pruebaCuentas: Cuentas[]=[
-    {noControl: 258,
-      Municipio: 'acajete',
-      NoMpio: 1,
-      tipoCuenta: 'urbano',
-      NoCuenta: 258963,
-      superficieTerreno: 789,
-      valorTerreno: 258,
-      valorTerrenoCalculado: 147,
-      superficieConstruccion: 963,
-      valorConstruccion: 123,
-      valorConstruccionCalculado: 147,
-      superficieObra: 789,
-      valorObra: 963,
-      valorComplementarioCalculado: 321,
-      baseGravable: 852,
-      baseGravableCalculada: 159,
-      IMPUESTO: 753,
-      impuestoAjustado: 654,
-      impuestoCalculado: 458,
-      difrencias: 25,
-      porcentaje: 78,
-      motivo: 'motivo'
-    },
-    {noControl: 258,
-      Municipio: 'acajeteDos',
-      NoMpio: 1,
-      tipoCuenta: 'urbanoDos',
-      NoCuenta: 258963,
-      superficieTerreno: 789,
-      valorTerreno: 258,
-      valorTerrenoCalculado: 147,
-      superficieConstruccion: 963,
-      valorConstruccion: 123,
-      valorConstruccionCalculado: 147,
-      superficieObra: 789,
-      valorObra: 963,
-      valorComplementarioCalculado: 321,
-      baseGravable: 852,
-      baseGravableCalculada: 159,
-      IMPUESTO: 753,
-      impuestoAjustado: 654,
-      impuestoCalculado: 458,
-      difrencias: 25,
-      porcentaje: 78,
-      motivo: 'motivo'
-    }
-  ];
+  cuentas: Cuentas[] = [];
   
-  /*obtenerDatos(){
+  obtenerDatos(){
     this.service.valuacionMunicipio('1',2024).subscribe((data: Object )=>{
       const padronData = data as Cuentas [];
       this.cuentas = padronData;
-      console.log(this.cuentas);
     });
-  }*/
+  }
 }
 
